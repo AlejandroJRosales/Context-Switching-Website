81e1446125133ad3bfdb14c66c601a99f39bb479
Alejandro Rosales <alejandrorosales479@gmail.com>
2022-09-20 04:27:44 +0000

    clean up footer, table of contents, manifold page UI

--- script.js
+++ script.js
@@ -6,22 +6,22 @@ $(function() {
 $(function() {
 	// var sectionNames = ["category-header", "section-header", "subsection-header"]
 
-	$(".table-of-contents").append("<h5>Table of Contents Links</h5>");
+	$(".table-of-contents").append("<h5>Links for Table of Contents</h5>");
 	$(".table-of-contents").append('<ol>');
-	
+
 	// loop through the whole DOM tree using find(*)
-	$(".information").find('*').each(function (index) {
+	$(".information").find('*').each(function(index) {
 		// get the section header name from the first paragraph tag
 		var currentSectionName = $(this).find("p:first").attr('class');
 		var lastSectionName = currentSectionName;
-		
+
 		// make sure the node we are looking at is a section and is not categories definer
 		if (typeof currentSectionName !== "undefined" && !$(this).hasClass('categories')) {
 			// console.log('section name: ' + currentSectionName);
 			// console.log($(this).find("p:first").text());
 
 			$(".table-of-contents").append('<li><a href="#' + $(this).find("p:first").attr('id') + '">' + $(this).find("p:first").text() + '</a></li>');
-			
+
 			// for (let nameIndex = 0; nameIndex < sectionNames.length; nameIndex++){
 			// 	if (currentSectionName == sectionNames[nameIndex]) {
 			// 		$(".table-of-contents").append($(this).find("p:first").text());
@@ -32,77 +32,77 @@ $(function() {
 	$(".table-of-contents").append('</ol>');
 });
 
-function tableContents() {}
+function tableContents() { }
 
 function isMobile() {
-    const toMatch = [
-        /Android/i,
-        /webOS/i,
-        /iPhone/i,
-        /iPad/i,
-        /iPod/i,
-        /BlackBerry/i,
-        /Windows Phone/i
-    ];
+	const toMatch = [
+		/Android/i,
+		/webOS/i,
+		/iPhone/i,
+		/iPad/i,
+		/iPod/i,
+		/BlackBerry/i,
+		/Windows Phone/i
+	];
 
-    return toMatch.some((toMatchItem) => {
-        return navigator.userAgent.match(toMatchItem);
-    });
+	return toMatch.some((toMatchItem) => {
+		return navigator.userAgent.match(toMatchItem);
+	});
 }
 
-$(document).ready(function(){
+$(document).ready(function() {
 	var paddingPxTop = String($(window).height() * .35) + "px";
 	var paddingPxBottom = String($(window).height() * .65) + "px";
-	$(".website-intro").css({"padding-top": paddingPxTop, "padding-bottom": paddingPxBottom});
+	$(".website-intro").css({ "padding-top": paddingPxTop, "padding-bottom": paddingPxBottom });
 });
 
-$(document).ready(function(){ 
-  if (isMobile()) {
-    // $(".topics-dropdown-menu").css({"padding-bottom": "2%"});
-    $(".nav-bar-brand").css({"font-size": "1em"});
-    $(".code").css({"font-size": "75%", "margin": "10% 3% 10% 3%"});
-    $(".collapsible-contents-button").css({"bottom": "7%", "right": "3%"});
-		$(".contents-button").css({"margin-left": "2%"});
-		$("#collapse").css({"margin-left": "2%"});
-    $(".information").css({"padding-left": "6%", "padding-right": "6%", "margin-left": "4%", "margin-right": "4%"});
-    $(".website-title").css({"font-size": "1.5em"});
-    $(".website-creator").css({"font-size": ".8em"});
-    $(".homepage-info").css({"font-size": ".73em"});
-		$("h1").css({"margin": "9% 0% 8% 0%"});
-		$("h4").css({"margin": "9% 0% 20% 0%"});
-    // $(".text-box").css({"max-height": "15em", "overflow:": "auto"});
-  }
+$(document).ready(function() {
+	if (isMobile()) {
+		// $(".topics-dropdown-menu").css({"padding-bottom": "2%"});
+		$(".nav-bar-brand").css({ "font-size": "1em" });
+		$(".code").css({ "font-size": "75%", "margin": "10% 3% 10% 3%" });
+		$(".collapsible-contents-button").css({ "bottom": "7%", "right": "3%" });
+		$(".contents-button").css({ "margin-left": "2%" });
+		$("#collapse").css({ "margin-left": "2%" });
+		$(".information").css({ "padding-left": "6%", "padding-right": "6%", "margin-left": "4%", "margin-right": "4%" });
+		$(".website-title").css({ "font-size": "1.5em" });
+		$(".website-creator").css({ "font-size": ".8em" });
+		$(".homepage-info").css({ "font-size": ".73em" });
+		$("h1").css({ "margin": "9% 0% 8% 0%" });
+		$("h4").css({ "margin": "9% 0% 20% 0%" });
+		// $(".text-box").css({"max-height": "15em", "overflow:": "auto"});
+	}
 });
 
 window.MathJax = {
-  tex: {
-    inlineMath: [['$', '$'], ['\\(', '\\)']]
-  }
+	tex: {
+		inlineMath: [['$', '$'], ['\\(', '\\)']]
+	}
 };
 
-$(document).ready(function() { 
-  $("button").click(function() { 
-    // Change text of button element
-    if ($("button").html() == "X") {
-      $("button").html("Table of Contents");
-    } 
-    if ($("button").html() == "Table of Contents") {
-      $("button").html("X");
-    } 
-  }); 
+$(document).ready(function() {
+	$("button").click(function() {
+		// Change text of button element
+		if ($("button").html() == "X") {
+			$("button").html("Table of Contents");
+		}
+		if ($("button").html() == "Table of Contents") {
+			$("button").html("X");
+		}
+	});
 });
 
-$(document).ready(function() { 
-  $("#pills-about-website").click(function() { 
-    // Change text of button element
-    console.log("clicked");
-  }); 
+$(document).ready(function() {
+	$("#pills-about-website").click(function() {
+		// Change text of button element
+		console.log("clicked");
+	});
 });
 
 function openSearch() {
-  document.getElementById("myOverlay").style.display = "block";
+	document.getElementById("myOverlay").style.display = "block";
 }
 
 function closeSearch() {
-  document.getElementById("myOverlay").style.display = "none";
+	document.getElementById("myOverlay").style.display = "none";
 }
