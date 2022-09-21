09366404e0ceae3b41b31fccb3f917cbc7ab48ab
Alejandro Rosales <alejandrorosales479@gmail.com>
2022-09-21 10:14:41 -0400

    cleaned up navbar, edited manifolds text, updated footer ui

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ $(function() {
 $(function() {
 	var sectionHeaders = ["category-header", "section-header", "subsection-header", "subsubsection-header"]
 
-	var tableOfContentsStr = "<h5>Links for Table of Contents</h5>";
+	var tableOfContentsStr = "<h5>Page Navigation Table</h5>";
 	var lastHeaderIdx = 0;
 	var currHeaderIdx = 0;
 
