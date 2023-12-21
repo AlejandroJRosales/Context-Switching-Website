ad634fb0cd6e1b2824b9e187ee5ea69b0e633dbf
AlejandroJRosales <alejandrorosales479@gmail.com>
2023-12-21 05:45:09 +0000

    updated size of homepage icons

--- assets/js/preset-divs.js
+++ assets/js/preset-divs.js
@@ -204,7 +204,7 @@ var navbar =
 	<li class="nav-item dropdown">
 				<a class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true"
 					aria-expanded="false">
-					More Topics
+					Miscellaneous
 				</a>
 				<div class="dropdown-menu topics-dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="/misc/adsandeinsteinframes">Anti-de Sitter Space and Einstein Frames</a>
