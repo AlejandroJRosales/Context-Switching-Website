251839d13ed9776308d592409003468f4d606647
AlejandroJRosales <alejandrorosales479@gmail.com>
2024-11-27 14:27:20 -0500

    update website title padding

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-	var paddingPxTop = String($(window).height() * .235) + "px";
-	var paddingPxBottom = String($(window).height() * .325) + "px";
+	var paddingPxTop = String($(window).height() * .18) + "px";
+	var paddingPxBottom = String($(window).height() * .25) + "px";
 	$(".website-intro").css({ "padding-top": paddingPxTop, "padding-bottom": paddingPxBottom });
 });
