62bc612bdcc74126129dac945b2adfc5c9e70b76
AlejandroJRosales <alejandrorosales479@gmail.com>
2025-09-17 22:31:37 -0400

    update website introduction text and adjust padding for improved layout

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-	var paddingPxTop = String($(window).height() * .17) + "px";
+	var paddingPxTop = String($(window).height() * .1) + "px";
 	var paddingPxBottom = String($(window).height() * .0) + "px";
 	$(".website-intro").css({ "padding-top": paddingPxTop, "padding-bottom": paddingPxBottom });
 });
