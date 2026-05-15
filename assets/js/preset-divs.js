35348635b37702b29a1a13e95891f205e072b2e4
AlejandroJRosales <alejandrorosales479@gmail.com>
2026-05-15 15:15:14 -0400

    added cv and updated resume

--- assets/js/preset-divs.js
+++ assets/js/preset-divs.js
@@ -265,8 +265,9 @@ var navbar =
 				</a>
 				<div class="dropdown-menu topics-dropdown-menu" aria-labelledby="navbarDropdown">
 					<a class="dropdown-item" href="/my/aboutme">About Me</a>
-					<a class="dropdown-item" href="/my/researchandwork">Research and Projects</a>
 					<a class="dropdown-item" target="_blank" rel="noopener noreferrer" href="/my/resume">Resume</a>
+					<a class="dropdown-item" target="_blank" rel="noopener noreferrer" href="/my/cv">CV</a>
+					<a class="dropdown-item" href="/my/researchandwork">Research and Projects</a>
 					<a class="dropdown-item" target="_blank" rel="noopener noreferrer" href="https://www.linkedin.com/in/alejandro-rosales-36ab16191/">LinkedIn</a>
 					<a class="dropdown-item" target="_blank" rel="noopener noreferrer" href="https://github.com/AlejandroJRosales">GitHub</a>
 					<a class="dropdown-item" href="/my/ecosim/">Ecosystem Simulation</a>
