18a4bfc112fc962bf319eda06845bc532d092d1d
AlejandroJRosales <alejandrorosales479@gmail.com>
2026-09-25 18:57:37 -0400

    Update navbar link text for clarity: change "Ecosystem Simulation" to "Ecosystem"

--- assets/js/preset-divs.js
+++ assets/js/preset-divs.js
@@ -169,7 +169,7 @@ var navbar =
 					<a class="dropdown-item" href="/my/researchandwork">Research and Projects</a>
 					<a class="dropdown-item" href="https://www.linkedin.com/in/alejandro-rosales-36ab16191/">LinkedIn</a>
 					<a class="dropdown-item" href="https://github.com/AlejandroJRosales">GitHub</a>
-					<a class="dropdown-item" href="/my/world/">Ecosystem Simulation</a>
+					<a class="dropdown-item" href="/my/world/">Ecosystem</a>
 					<a class="dropdown-item" href="/my/game/">Game</a>
 				</div>
 			</li>
